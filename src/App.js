6db10c6682cce78ca6bbda4f7cf8f53a91526e49
Muhammad Shafaqat <muhammadshafaqat0344@gmail.com>
2024-06-06 00:04:19 +0500

    filter page conditional rendring

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import { Navbar1 } from "./components/navbar1/Navbar1";
 import { Home } from "./components/home/Home";
 import { Filters } from "./components/filters/Filters";
@@ -12,27 +12,39 @@ import Footer from "./components/footer/Footer";
 
 function App() {
   const [category, setCategory] = useState('all');
-  const [searchTerm, setSearchTerm] = useState('')
+  const [searchTerm, setSearchTerm] = useState('');
 
   return (
     <React.Fragment>
-   <Router >
-      <div className="App1">
-<Navbar1 setSearchTerm={setSearchTerm} />
-<Filters setCategory={setCategory} />
-<Routes>
-
-      <Route exact path='/' element={<Home category={category}  searchTerm={searchTerm} />}  />
-      <Route path='/product/:id' element={<Product/>}  />
-      <Route   path='/cartItems' element={<CartItems/>}  />
-      <Route   path='/checkoutPage' element={<CheckoutPage/>}  />
-
-</Routes>
-<Footer />
-      </div>
+      <Router>
+        <div className="App1">
+          <Navbar1 setSearchTerm={setSearchTerm} />
+          <MainContent category={category} setCategory={setCategory} searchTerm={searchTerm} />
+          <Footer />
+        </div>
       </Router>
     </React.Fragment>
   );
 }
+
+const MainContent = ({ category, setCategory, searchTerm }) => {
+  const location = useLocation();
+
+  // Define routes where you don't want to show the Filters component
+  const hideFilterRoutes = ['/cartItems', '/checkoutPage'];
+
+  return (
+    <>
+      {/* Conditionally render Filters component */}
+      {!hideFilterRoutes.includes(location.pathname) && <Filters setCategory={setCategory} />}
+      <Routes>
+        <Route exact path='/' element={<Home category={category} searchTerm={searchTerm} />} />
+        <Route path='/product/:id' element={<Product />} />
+        <Route path='/cartItems' element={<CartItems />} />
+        <Route path='/checkoutPage' element={<CheckoutPage />} />
+      </Routes>
+    </>
+  );
+};
 
 export default App;
